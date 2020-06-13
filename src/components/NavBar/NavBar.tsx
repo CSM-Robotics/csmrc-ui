bfb64ac58d0d3f0dc15f96eda51dfeb204c41313
PrefixTim <developer.of.gift@gmail.com>
2020-06-12 20:29:22 -0700

    tabs added

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,25 +2,66 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import {createStyles, Theme, WithStyles, withStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import logo from "../../images/logo.png"
 import "./NavBar.css"
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const NavBar = () => {
-    return (
-        <div>
-            <AppBar position="static" className="NavBar">
-                <Toolbar>
-                    <img src={logo} className="Bar-logo" alt="logo"/>
-                    <Typography variant="h6" color="inherit" >
-                        CSM Robotics Club
-                    </Typography>
-                    {/*<Typography variant="subtitle1" color="">*/}
-                    {/*    Test2*/}
-                    {/*</Typography>*/}
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
+const styles = (theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 5,
+            backgroundColor: theme.palette.background.paper,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 5,
+        },
+    });
+
+class NavBar extends React.Component<WithStyles<typeof styles>> {
+    state = {
+        value: 0
+    }
+
+
+    handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        this.setState({value: newValue})
+    }
+
+    render() {
+        const {classes} = this.props;
+        const {value} = this.state;
+
+        return (
+            <div>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <img src={logo} className="Bar-logo" alt="logo"/>
+                        <Typography variant="h6" className={classes.title}>
+                            CSM Robotics Club
+                        </Typography>
+                        <Tabs value={value} onChange={this.handleChange}>
+                            <Tab label="Item One"/>
+                            <Tab label="Item Two"/>
+                            <Tab label="Item Three"/>
+                        </Tabs>
+                        <Button color="inherit">Login</Button>
+
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
 }
 
-export default NavBar;
+export default withStyles(styles)(NavBar);
