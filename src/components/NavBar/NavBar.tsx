32bb8aee746e333f3bc0685d9666429e6f1da5a8
PrefixTim <developer.of.gift@gmail.com>
2020-06-12 18:05:38 -0700

    text changed

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-// import logo from "../../images/logo.png"
+import logo from "../../images/logo.png"
 import "./NavBar.css"
 
 const NavBar = () => {
@@ -10,9 +10,9 @@ const NavBar = () => {
         <div>
             <AppBar position="static" className="NavBar">
                 <Toolbar>
-                    {/*<img src={logo} className="Bar-logo"/>*/}
-                    <Typography variant="subtitle1" color="inherit">
-                        Test
+                    <img src={logo} className="Bar-logo" alt="logo"/>
+                    <Typography variant="h6" color="inherit" >
+                        CSM Robotics Club
                     </Typography>
                     {/*<Typography variant="subtitle1" color="">*/}
                     {/*    Test2*/}
