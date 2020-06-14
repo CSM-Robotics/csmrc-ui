7ba6ebe2b301eee4ab3402d170893c8e8b4daa41
PrefixTim <developer.of.gift@gmail.com>
2020-06-13 17:50:07 -0700

    simple page added

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class AboutUs extends Component<{}, any> {
+
+    render() {
+
+        return (
+            <div style={{height: '100%', width: '100%'}}>
+                <p>
+                    projects here
+                </p>
+            </div>
+        );
+    }
+}
+
+export default AboutUs;
