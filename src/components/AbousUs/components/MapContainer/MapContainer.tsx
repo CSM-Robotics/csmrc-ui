a712d6b0a81d6c4c9d60621ff99ce13822b45cf1
PrefixTim <developer.of.gift@gmail.com>
2020-06-13 17:38:38 -0700

    simple about us page with map

---
+++ src/components/AbousUs/components/MapContainer/MapContainer.tsx
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+
+class MapContainer extends Component<{width?:number, height?:number, zoom?:number}, any> {
+    static defaultProps = {
+        width:480,
+        height:720,
+        zoom:15
+    }
+    constructor(props:{width:number, height:number, zoom:number}) {
+        super(props);
+        this.state = {
+            key:"AIzaSyAoOkFK3Cs4HdeQTLXhb6fwBgSVpUMCqWc",
+            location:"Library+and+KCSM+Building+9,+1700+W+Hillsdale+Blvd,+San+Mateo,+CA+94402",
+        }
+    }
+
+    render() {
+        const {key, location} = this.state;
+        const {width, height, zoom} = this.props;
+        return (
+            <div>
+                <iframe
+                    className="about-map"
+                    title="about-map"
+                    width={width}
+                    height={height}
+                    frameBorder={0}
+                    src={`https://www.google.com/maps/embed/v1/place?key=${key}&q=${location}&zoom=${zoom}`} allowFullScreen>
+                </iframe>
+            </div>
+        );
+    }
+}
+
+export default MapContainer;
